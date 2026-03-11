e9eb994fe9d1ef82615c6166aca3c742ecc4d11c
jaehyun00 <ski03205@gmail.com>
2026-03-11 23:28:26 +0900

    20260311_dp_8-8_수정

--- algorithm/dynamic_programing/8-8.py
+++ algorithm/dynamic_programing/8-8.py
@@ -55,7 +55,7 @@ d = [10001] * (m + 1)
 # 다이나믹 프로그래밍 진행(보텀업)
 d[0] = 0
 for i in range(n):
-    for j in range(array[j], m + 1):
+    for j in range(array[i], m + 1):
         if d[j - array[i]] != 10001: # (i - k)원을 만드는 방법이 존재하는 경우
             d[j] = min(d[j], d[j - array[i]] + 1)
 
